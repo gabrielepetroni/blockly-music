172c2bf166b2c81e740bafd84dec9749ab10012f
Gabriele <gabriele.petroni2k@gmail.com>
2023-03-29 22:48:18 +0200

    Using complete-code

--- scripts/main.js
+++ scripts/main.js
@@ -8,11 +8,30 @@
   let currentButton;
 
   function handlePlay(event) {
-    // Add code for playing sound.
+    loadWorkspace(event.target);
+    let code = Blockly.JavaScript.workspaceToCode(Blockly.getMainWorkspace());
+    code += 'MusicMaker.play();';
+    // Eval can be dangerous. For more controlled execution, check
+    // https://github.com/NeilFraser/JS-Interpreter.
+    try {
+      eval(code);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function loadWorkspace(button) {
+    const workspace = Blockly.getMainWorkspace();
+    if (button.blocklySave) {
+      Blockly.serialization.workspaces.load(button.blocklySave, workspace);
+    } else {
+      workspace.clear();
+    }
   }
 
   function save(button) {
-    // Add code for saving the behavior of a button.
+    button.blocklySave = Blockly.serialization.workspaces.save(
+        Blockly.getMainWorkspace());
   }
 
   function handleSave() {
@@ -39,6 +58,7 @@
   function enableBlocklyMode(e) {
     document.body.setAttribute('mode', 'blockly');
     currentButton = e.target;
+    loadWorkspace(currentButton);
   }
 
   document.querySelector('#edit').addEventListener('click', enableEditMode);
@@ -47,4 +67,34 @@
 
   enableMakerMode();
 
+  const toolbox = {
+    'kind': 'flyoutToolbox',
+    'contents': [
+      {
+        'kind': 'block',
+        'type': 'controls_repeat_ext',
+        'inputs': {
+          'TIMES': {
+            'shadow': {
+              'type': 'math_number',
+              'fields': {
+                'NUM': 5,
+              },
+            },
+          },
+        },
+      },
+      {
+        'kind': 'block',
+        'type': 'play_sound',
+      },
+    ],
+  };
+
+  Blockly.inject('blocklyDiv', {
+    toolbox: toolbox,
+    scrollbars: false,
+    horizontalLayout: true,
+    toolboxPosition: 'end',
+  });
 })();
